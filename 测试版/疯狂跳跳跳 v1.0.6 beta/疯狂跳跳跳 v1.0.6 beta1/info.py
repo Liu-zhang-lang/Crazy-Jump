fa6f51c3418eeb0b5bc027ecb2d40758fef88800
Liu-zhang-lang <mike_lyh@qq.com>
2026-03-23 22:08:55 +0800

    update

--- 测试版/疯狂跳跳跳 v1.0.6 beta/疯狂跳跳跳 v1.0.6 beta1/info.py
+++ 测试版/疯狂跳跳跳 v1.0.6 beta/疯狂跳跳跳 v1.0.6 beta1/info.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from const import *
-class Information(pg.sprite.Sprite):
+class Info(pg.sprite.Sprite):
     def __init__(self,inf_string,right_top_pos,col):
         super().__init__()
         self.image=chinese_font.render(inf_string,True,col)
@@ -11,8 +11,8 @@ class Information(pg.sprite.Sprite):
         if self.time<=0:
             self.kill()
     @staticmethod
-    def add_inf(inf,inf_string,col):
-        for i in inf:
+    def add_info(info,info_string,col):
+        for i in info:
             i.rect.top+=chinese_font_size
-        inf.add(Information(inf_string,[w,chinese_font_size],col))
-        return inf
+        info.add(Info(info_string,[w,chinese_font_size],col))
+        return info
