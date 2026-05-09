7953bd92ed55290c0fffb56443902bf0d45c6c9f
Liu-zhang-lang <mike_lyh@qq.com>
2026-05-09 21:46:18 +0800

    更新v1.0.8

---
+++ 正式版/疯狂跳跳跳 v1.0.8/const.py
@@ -0,0 +1,96 @@
+import pygame as pg
+import os
+import inspect as ins
+pg.init()
+version="v1.0.8"
+format=2
+w,h=800,600
+level_up_time=[
+    [15,17],
+    [30,32],
+    [45,47],
+    [70,72],
+    [90,92],
+    [150,152]
+]
+all_pro=[
+    0.008,
+    0.016,
+    0.032,
+    0.042,
+    0.052,
+    0.068
+]
+all_hpro=[
+    0.0015,
+    0.003,
+    0.004,
+    0.006,
+    0.007,
+    0.0075
+]
+all_fpro=[
+    0.0005,
+    0.001,
+    0.00125,
+    0.0014,
+    0.0017,
+    0.0018
+]
+all_rpro=[
+    0,
+    0.002,
+    0.004,
+    0.01,
+    0.03,
+    0.045
+]
+clock=pg.time.Clock()
+window=pg.display.set_mode((w,h))
+font_file=os.path.join(os.path.dirname(__file__),"fonts","simsun.ttc") #加载字体
+cn_sm=pg.font.Font(font_file,20)
+cn_sm_sz=20
+cn_def=pg.font.Font(font_file,25)
+cn_def_sz=25
+cn_big=pg.font.Font(font_file,45)
+cn_big_sz=45
+cn_fonts=[]
+cn_fonts_sz=[]
+for i in range(1,100):
+    cn_fonts.append(pg.font.Font(font_file,i))
+    cn_fonts_sz.append(i)
+info_st_pos=(w,cn_def_sz*3)
+gray_img=pg.Surface((w,h),pg.SRCALPHA)
+gray_img.fill((128,128,128,206))
+slogans=[
+    "疯狂跳跳跳！",
+    "这怪怎么会变向？？？",
+    "Ohhhhhhhhhh！",
+    "zyc=666！",
+    "蛋糕店里卖蛋糕！",
+    "Do you know chinglish？",
+    "我爱C++！",
+    "MC真好玩！",
+    "猜猜你多久才能再次看到这个标语？",
+    "这什么逆天bug？",
+    "Hello World!",
+    "x=x+2,x=-1？",
+    "3m=2m,3m/m=2m/m,3=2？",
+    "这土豆长得好像马铃薯啊！",
+    "if(false) cout<<ans;",
+    "well的中文：井",
+    "super idol的笑容~"
+]
+is_info=False
+for s in ins.stack()[::-1]:
+    if os.path.basename(s.filename)=="info.py":
+        is_info=True
+        break
+if not is_info:
+    from info import Info
+    fst_pos=[w/2,h/2-60]
+    gamestop_info=pg.sprite.Group()
+    gamestop_info=Info.append_info(gamestop_info,"游戏暂停",fst_pos,"center",cn_big,time=-1,spacing=5,mode="back")
+    gamestop_info=Info.append_info(gamestop_info,"按Esc或Enter继续游戏",fst_pos,"center",time=-1,spacing=5,mode="back")
+    gamestop_info=Info.append_info(gamestop_info,"按R重新开始",fst_pos,"center",time=-1,spacing=5,mode="back")
+    gamestop_info=Info.append_info(gamestop_info,"按G查看规则",fst_pos,"center",time=-1,spacing=5,mode="back")
---
+++ 正式版/疯狂跳跳跳 v1.0.8/health.py
@@ -0,0 +1,45 @@
+import pygame as pg
+import random as rd
+from const import *
+class Health(pg.sprite.Sprite):
+    def __init__(self,f=0,x=-1,y=-1,h=-1,draw_num=False):
+        super().__init__()
+        if (rd.random()<=0.05 and f!=1) or f==2:
+            self.b=40
+        else:
+            self.b=rd.randint(15,35)
+        if x==-1:
+            x=rd.randint(0,w-self.b)
+        if y==-1:
+            y=rd.randint(200,450)
+        if h==-1:
+            if self.b!=40:
+                self.h=self.b//3
+            else:
+                self.h=25
+        else:
+            self.h=h
+        if self.b!=40:
+            self.number_text=cn_def.render(str(self.h),True,"white")
+        else:
+            self.number_text=cn_def.render(str(self.h),True,"red")
+        self.img_w,self.img_h=self.number_text.get_size()
+        self.t=min(self.b/self.img_w,self.b/self.img_h)
+        self.img_w*=self.t
+        self.img_h*=self.t
+        self.img_w=int(self.img_w)
+        self.img_h=int(self.img_h)
+        self.number_text=pg.transform.scale(self.number_text,(self.img_w,self.img_h))
+        self.rect=pg.Rect((x,y),(self.b,self.b))
+        self.image=pg.Surface((self.b,self.b))
+        if self.b!=40:
+            pg.draw.rect(self.image,"green",(0,0,self.b,self.b))
+        else:
+            pg.draw.rect(self.image,"yellow",(0,0,self.b,self.b))
+        self.health_img=self.image.copy()
+        if draw_num:
+            self.draw_pic(True)
+    def draw_pic(self,show_number=False):
+        self.image=self.health_img.copy()
+        if show_number:
+            self.image.blit(self.number_text,self.number_text.get_rect(center=(self.b/2,self.b/2)))
